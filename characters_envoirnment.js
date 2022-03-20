b6dd2f261232014a7c7331c1a5dff90b7fc7d7ea
SoumyaCoding <86145656+SoumyaCoding@users.noreply.github.com>
2022-03-20 06:53:10 -0700

    Add files via upload

---
+++ characters_envoirnment.js
@@ -0,0 +1,116 @@
+
+/*=================================
+=            Variables            =
+=================================*/
+
+/* main character variabes */
+var mario, bricks,clouds,mountains,enemyMushrooms,pipes,platforms,coins;
+
+/* Control variabes */
+var control={
+  up: "UP_ARROW", // 32=spaceBar
+  left: 'LEFT_ARROW',
+  right: 'RIGHT_ARROW',
+  revive: 32
+}
+
+//Inner game status, which might affect game balance or playability.
+var gameConfig={
+  
+  // start, playing, over
+  status: "start", 
+  
+  // initial lives of mario
+  initialLifes: 4,
+
+  // character moves speed
+  moveSpeed: 5,
+  enemyMoveSpeed: 1,
+
+  // gravity and jump speed for all the characters
+  gravity: 1,
+  gravityEnemy: 10,
+  jump:-15,
+
+  // character starting point
+  startingPointX: 500,
+  startingPointY: 0,
+
+  // default canvas size
+  screenX:1240,
+  screenY:336,
+
+  // scores
+  timeScores: 0,
+  scores: 0
+}
+
+
+/*=====  End of Variables  ======*/
+
+
+/*====================================
+=            Game Status             =
+====================================*/
+noseX = "";
+noseY = "";
+GameStatus = "";
+
+function game(){
+
+  console.log("noseX = " + noseX +" ,noseY =  "+ noseY);
+
+  instializeInDraw();
+  moveEnvironment(mario);
+  drawSprites();
+  
+  if(gameConfig.status==='start'){
+
+    fill(0,0,0,150);
+    rect(0,0,gameConfig.screenX,gameConfig.screenY);
+
+    fill(255, 255, 255);
+    textSize(40);
+    textAlign(CENTER);
+    text("Press Play Button To Start The Game ", gameConfig.screenX/2, gameConfig.screenY/2);
+    textSize(40);
+
+    stroke(255);
+    strokeWeight(7);
+    noFill();
+
+    changeGameStatud();
+  }
+  
+  if(gameConfig.status==='play'){
+    positionOfCharacter(mario);
+    enemys(enemyMushrooms);
+    checkStatus(mario);
+    scores(mario);
+    manualControl(mario);
+  
+  }
+
+    // if game is over 
+  if(gameConfig.status==='gameover'){
+    
+    fill(0,0,0,150);
+    rect(0,0,gameConfig.screenX,gameConfig.screenY);
+
+    fill(255, 255, 255);
+    textSize(40);
+    textAlign(CENTER);
+    text("GAME OVER", gameConfig.screenX/2, gameConfig.screenY/2+105);
+    textSize(15);
+    text("Press SPACE to Restart", gameConfig.screenX/2, gameConfig.screenY/2+135);
+    textSize(40);
+    text(round(gameConfig.scores),gameConfig.screenX/2,gameConfig.screenY/2-35);
+    text("points",gameConfig.screenX/2,gameConfig.screenY/2);
+
+    stroke(255);
+    strokeWeight(7);
+    noFill();
+    ellipse(gameConfig.screenX/2,gameConfig.screenY/2-30,160,160)
+    changeGameStatus(mario)
+  }
+}  
